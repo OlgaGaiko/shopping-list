15d3135c051d8a5bec9963119862c659ce1e1198
OlgaGaiko <164740336+OlgaGaiko@users.noreply.github.com>
2024-04-07 21:07:43 +0300

    домашнее задание 4

--- script.js
+++ script.js
@@ -1,3 +1,36 @@
+const sendInput = document.querySelector('#input');
+const messagesContainer = document.querySelector('.input-wrapper');
+const elements = document.querySelectorAll('#items');
+
+
+sendInput.addEventListener('keydown', function (event){
+    const messageText = sendInput.value;
+
+    for (let item of elements){
+        item.addEventListener('click', function(){
+            item.classList.toggle ('done');
+          }) 
+        }
+   
+   
+    if (event.key== 'Enter') {
+   
+    }
+
+    if (messageText !=''){
+        messagesContainer.append(elements);
+    }
+    
+    sendInput.value ='';
+    
+})
+
+
+ 
+
+ 
+
+
 /* Новые элементы должны добавляться в список по нажатию на Enter */
 
 /* Пустые элементы не должны добавляться */
